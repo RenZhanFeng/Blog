8b5b44ec5f671623107ed170fb2731ae7db30932
clown <z_jpeng@qq.com>
2020-04-21 15:24:57 +0800

    整合数据库联调

--- managerSystem/ManagerSys/src/main/java/cr/ms/pojo/User.java
+++ managerSystem/ManagerSys/src/main/java/cr/ms/pojo/User.java
@@ -1,8 +1,24 @@
 package cr.ms.pojo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity //表示这是一个实体类
+@Table(name = "user")//表示对应的表名是 user
+@JsonIgnoreProperties({"handler","hibernateLazyInitializer"})//忽略无须 json 化的属性
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Integer id;
+	
 	private String username;
 	private String password;
 	
