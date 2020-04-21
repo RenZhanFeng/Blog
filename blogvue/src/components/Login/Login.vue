1f739fad9553145163ca4a2177c7e01fb39cbe55
RenZhanFeng <947625259@qq.com>
2020-04-21 11:55:11 +0800

    '前后端联调测试'

--- blogvue/src/components/Login/Login.vue
+++ blogvue/src/components/Login/Login.vue
@@ -29,20 +29,11 @@ export default {
   },
   methods: {
     login() {
-      // this.$axios
-      //   .post("/login", {
-      //     username: this.loginForm.username,
-      //     password: this.loginForm.password
-      //   })
-      //   .then(successResponse => {
-      //     if (successResponse.data.code === 200) {
-      //       this.$router.replace({ path: "/index" });
-      //     }
-      //   });
       this.$axios
-        .get(
-          `login2?username=${this.loginForm.username}&password=${this.loginForm.password}`
-        )
+        .post("/login", {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
         .then(successResponse => {
           if (successResponse.data.code === 200) {
             this.$router.replace({ path: "/index" });
