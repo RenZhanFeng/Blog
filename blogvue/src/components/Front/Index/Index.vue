9838a7d38f6614218c523a9cbc016bd4ccc3279a
RenZhanFeng <947625259@qq.com>
2020-04-29 00:17:27 +0800

    制作前台博客页面布局

--- blogvue/src/components/Front/Index/Index.vue
+++ blogvue/src/components/Front/Index/Index.vue
@@ -1,23 +1,55 @@
 <template>
   <el-row class="index">
     <el-container>
-      <el-col class="userInof" :xs="0" :sm="3" :md="3" :lg="3" style="background:#ccc"></el-col>
-      <el-col class="userInof" :xs="0" :sm="4" :md="4" :lg="4" style="background:#000">1</el-col>
-      <el-col class="blogList" :xs="0" :sm="10" :md="10" :lg="10" style="background:#fff">2</el-col>
-      <el-col class="blogCategory" :xs="0" :sm="4" :md="4" :lg="4" style="background:#000">3</el-col>
-      <el-col class="userInof" :xs="0" :sm="3" :md="3" :lg="3" style="background:#ccc"></el-col>
+      <el-col :xs="0" :sm="0" :md="0" :lg="0" :xl="2"></el-col>
+      <el-col class="userInof card" :xs="0" :sm="7" :md="8" :lg="6">
+        <el-row>
+          <el-col :span="24" class="item">qwe</el-col>
+        </el-row>
+      </el-col>
+      <el-col class="blogList card" :xs="24" :sm="17" :md="16" :lg="13">
+        <el-row>
+          <el-col :span="24" class="item">qwe</el-col>
+          <el-col :span="24" class="item">qwe</el-col>
+          <el-col :span="24" class="item">qwe</el-col>
+          <el-col :span="24" class="item">qwe</el-col>
+        </el-row>
+      </el-col>
+      <el-col class="blogCategory card" :xs="0" :sm="0" :md="0" :lg="5">
+        <el-row>
+          <el-col :span="24" class="item">qwe</el-col>
+          <el-col :span="24" class="item">qwe</el-col>
+        </el-row>
+      </el-col>
+      <el-col :xs="0" :sm="0" :md="0" :lg="0" :xl="2"></el-col>
     </el-container>
   </el-row>
 </template>
 
 <script>
 export default {
-  name: "Index"
+  name: "Index",
+  data() {
+    return {};
+  }
 };
 </script>
 
-<style scoped>
-.idnex {
+<style scoped lang='stylus'>
+@import '~stylus/mixin';
+
+.index {
+  margin-top: 40px;
+}
+
+.card {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.item {
+  margin-bottom: 20px;
   padding: 20px;
+  Shadow();
 }
 </style>
