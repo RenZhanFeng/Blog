93f0b60f3bb7ee0fd3aa13ff985336f8a5cec47a
RenZhanFeng <947625259@qq.com>
2020-04-29 00:25:13 +0800

    ..

--- blogvue/src/components/Front/Index/Index.vue
+++ blogvue/src/components/Front/Index/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <el-row class="index">
     <el-container>
-      <el-col :xs="0" :sm="0" :md="0" :lg="0" :xl="2"></el-col>
-      <el-col class="userInof card" :xs="0" :sm="7" :md="8" :lg="6">
+      <el-col :xs="0" :sm="1" :md="1" :lg="3"></el-col>
+      <el-col class="userInof card" :xs="0" :sm="5" :md="5" :lg="5">
         <el-row>
           <el-col :span="24" class="item">qwe</el-col>
         </el-row>
       </el-col>
-      <el-col class="blogList card" :xs="24" :sm="17" :md="16" :lg="13">
+      <el-col class="blogList card" :xs="24" :sm="13" :md="13" :lg="10">
         <el-row>
           <el-col :span="24" class="item">qwe</el-col>
           <el-col :span="24" class="item">qwe</el-col>
@@ -15,13 +15,13 @@
           <el-col :span="24" class="item">qwe</el-col>
         </el-row>
       </el-col>
-      <el-col class="blogCategory card" :xs="0" :sm="0" :md="0" :lg="5">
+      <el-col class="blogCategory card" :xs="0" :sm="4" :md="4" :lg="3">
         <el-row>
           <el-col :span="24" class="item">qwe</el-col>
           <el-col :span="24" class="item">qwe</el-col>
         </el-row>
       </el-col>
-      <el-col :xs="0" :sm="0" :md="0" :lg="0" :xl="2"></el-col>
+      <el-col :xs="0" :sm="1" :md="1" :lg="3"></el-col>
     </el-container>
   </el-row>
 </template>
